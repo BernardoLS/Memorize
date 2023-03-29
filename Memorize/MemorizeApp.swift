5e3c8498e852cdd55a36de7ca10686d4e61231de
Laguna <bernardo.laguna@taglivros.com.br>
2023-03-29 08:22:35 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by TagLivros on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
