844a120642cc321283bde7eaf3b85dcf733f6cd9
Bernardo Laguna dos Santos <47367588+BernardoLS@users.noreply.github.com>
2023-03-30 13:49:06 -0300

    lecture 2
    Improve layout, add grid and list of emotes, also create a model to be used in future.
    Add two buttons that remove and add new cards on screen

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-                .foregroundColor(.red)
-            Text("Hello, world!").foregroundColor(.orange)
-        }
-        .padding(.horizontal)
+    
+    struct CardContent: Identifiable {
+        var id: String
+        var content: String
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    let cardContents: [CardContent] =
+    [
+        CardContent(id:"01", content: "🏎"),
+        CardContent(id:"02", content: "🚀"),
+        CardContent(id:"03", content: "🏎"),
+        CardContent(id:"04", content: "🚀"),
+        
+    ]
+    
+    @State var emojisCount = 3
+    
+    let emojis: [String] = [
+        "🏎","🚀", "🚒", "🚑", "🛸", "✈️", "🚁", "🚜", "🚔", "🛶",
+        "🚗", "🚛", "🛵", "🚂", "🚌", "🛴", "🚲", "🚤", "🚚", "🏍"
+    ]
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojisCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                removeButton
+                Spacer()
+                addButton
+            }.font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .foregroundColor(.red)
+        .padding(.horizontal)
+        
+    }
+    var removeButton: some View {
+        Button(
+            action: {
+                if emojisCount > 1 {
+                    emojisCount -= 1
+                }
+            },
+            label: { Image(systemName: "minus.circle") }
+        )
+    }
+    
+    var addButton: some View {
+        Button(action: {
+            if emojisCount < emojis.count {
+                emojisCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+    }
+    
+    struct CardView: View {
+        var content: String
+        @State var isFaceUp = false
+        
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20)
+                
+                if isFaceUp {
+                    shape.foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                } else {
+                    shape
+                }
+                
+            }
+            .onTapGesture {
+                isFaceUp = !isFaceUp
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
