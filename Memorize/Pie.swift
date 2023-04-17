ea24b7c2b2f72923c80389d4ed97bf837791b3de
Bernardo Laguna dos Santos <47367588+BernardoLS@users.noreply.github.com>
2023-04-17 17:14:32 -0300

    add pie to start animation

---
+++ Memorize/Pie.swift
@@ -0,0 +1,36 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Laguna.Rkz on 17/04/23.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    func path(in rect: CGRect) -> Path {
+        let center: CGPoint = CGPoint(x: rect.midX, y: rect.midY)
+        let radius: CGFloat = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: !clockwise
+        )
+        p.addLine(to: center)
+        return p
+    }
+}
